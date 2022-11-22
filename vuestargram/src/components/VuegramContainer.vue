258a7c78b4fae1536198677c2fefa9a97f36eec4
daramjiyun <315ljy@gmail.com>
2022-11-22 23:10:41 +0900

    Create Layout

---
+++ vuestargram/src/components/VuegramContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <VuegramPost />
+    <VuegramPost />
+    <VuegramPost />
+  </div>
+</template>
+
+<script>
+import VuegramPost from './VuegramPost.vue';
+
+export default {
+  components : {
+    VuegramPost,
+  }
+}
+</script>
+
+<style>
+
+</style>
